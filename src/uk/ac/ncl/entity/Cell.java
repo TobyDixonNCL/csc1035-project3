cf0b7c174d6337de69238894f269d66212a57d7c
Toby Dixon <thetjdixon1234@gmail.com>
2021-05-05 20:32:31 +0100

    Changed isLegal method in Cell.java, first movement in direction should land on opponent tile for it to be a legal move.

--- src/uk/ac/ncl/entity/Cell.java
+++ src/uk/ac/ncl/entity/Cell.java
@@ -112,6 +112,13 @@ public class Cell {
      * @return whether move is possible for the piece. If this is the case, then possible moves are stored in Piece.
      */
     public boolean isLegal(CellStatus colour, Cell[][] cells) {
+
+        /*
+        This method takes the cell, with the colour of the current player, and an array of all current moves, and
+        evaluates whether or not the cell has any legal moves. It also pairs those moves with a score, and stores
+        them as a directed move.
+         */
+
         CellStatus opponent = colour == CellStatus.LIGHT ? CellStatus.DARK : CellStatus.LIGHT;
         boolean isLegal = false;
         int score = 0;
@@ -120,24 +127,24 @@ public class Cell {
 
         for (int[] dir : DIRS) {
             int temp_score = 0;
-            int d_row = this.getRow() + dir[0];
-            int d_col = this.getColumn() + dir[1];
-            if (0 <= d_col && d_col < BOARD_SIZE && 0 <= d_row && d_row < BOARD_SIZE
-                    && cells[d_row][d_col].getValue() != CellStatus.EMPTY
-                    && cells[d_row][d_col].getValue() != opponent) {
-                while (true) {
-                    d_row += dir[0];
-                    d_col += dir[1];
-                    temp_score += 1;
+            int d_row = this.getRow() + dir[0]; // Stores the first movement with direction (x)
+            int d_col = this.getColumn() + dir[1]; // Stores the first movement with direction (y)
+            if (0 <= d_col && d_col < BOARD_SIZE && 0 <= d_row && d_row < BOARD_SIZE // If on the board
+                    && cells[d_row][d_col].getValue() == opponent) { // If space doesnt belong to opponent
+                while (true) { // If the first movement is made and can work then:
+                    d_row += dir[0]; // Continue movement
+                    d_col += dir[1]; // Continue movement
+                    temp_score += 1; // Increase score each time you continue
                     if (0 <= d_col && d_col < BOARD_SIZE && 0 <= d_row && d_row < BOARD_SIZE
-                            && cells[d_row][d_col].getValue() != CellStatus.EMPTY) {
-                        if (cells[d_row][d_col].getValue() == colour) {
+                            && cells[d_row][d_col].getValue() != CellStatus.EMPTY) { // Check that you are still on the board and if you haven't hit an empty tile
+                        if (cells[d_row][d_col].getValue() == colour) { // if the tile you are at is the same colour as your team then finish
                             isLegal = true;
                             score = temp_score;
                             moves.add(new DirectedMove(cells[d_row][d_col], dir));
                         }
                     } else {
-                        break;
+                        break; // If an empty tile is hit, or the board ends then break from the loop
+
                     }
                 }
             }
